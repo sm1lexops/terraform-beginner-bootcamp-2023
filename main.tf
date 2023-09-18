20c0325b27f78b2b93114c25df8bd15613e80023
Aleksey Smirnov <62427708+sm1lexops@users.noreply.github.com>
2023-09-18 21:38:09 +0300

    add aws s3, implement random naming (#8)

--- main.tf
+++ main.tf
@@ -1,20 +1,34 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
     }
   }
 }
 
+provider "aws" {
+  
+}
 provider "random" {
   # Configuration options
 }
 
-resource "random_string" "backet_name" {
-    length      = 16
-    special     = false
+resource "aws_s3_bucket" "my_random_s3_bucket" {
+  bucket = "my-random-${random_string.bucket_name.id}"
 }
-output "random_backet_name_string" {
-    value = random_string.backet_name.id
+resource "random_string" "bucket_name" {
+  length  = 16
+  special = false
+  upper   = false
+}
+output "random_bucket_name_string" {
+  value = random_string.bucket_name.id
+}
+output "my_random_s3_bucket" {
+  value = aws_s3_bucket.my_random_s3_bucket.id
 }
