d01395b05fd418905d0914de374e4fe505d8c1a1
Aleksey Smirnov <62427708+sm1lexops@users.noreply.github.com>
2023-10-09 21:48:57 +0300

    implement terratowns-provider block (#32)

--- outputs.tf
+++ outputs.tf
@@ -2,17 +2,17 @@
 # Outputs
 ################################################################################
 
-output "aws_s3_bucket_name" {
-  description   = "Name of S3 bucket"
-  value         = module.terrahouse.aws_s3_bucket_id
-}
-
-output "aws_s3_website_endpoint" {
-  description = "AWS S3 Website Endpoint"
-  value = module.terrahouse.website_endpoint
-}
-
-output "cloudfront_url" {
-  description = "CloudFront URL"
-  value = module.terrahouse.cloudfront_url
-}
+#output "aws_s3_bucket_name" {
+#  description   = "Name of S3 bucket"
+#  value         = module.terrahouse.aws_s3_bucket_id
+#}
+#
+#output "aws_s3_website_endpoint" {
+#  description = "AWS S3 Website Endpoint"
+#  value = module.terrahouse.website_endpoint
+#}
+#
+#output "cloudfront_url" {
+#  description = "CloudFront URL"
+#  value = module.terrahouse.cloudfront_url
+#}
--- versions.tf
+++ versions.tf
@@ -2,20 +2,28 @@
 # Verion(s) and Requirement(s)
 ################################################################################
 terraform {
-    required_version = ">= 1.5.0"
 
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = ">= 5.17.0"
-      }
-    }
-
-    cloud {
-    organization = "thevopz"
-
-    workspaces {
-      name = "root-tf"
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
     }
   }
+#    required_version = ">= 1.5.0"
+#
+#    required_providers {
+#      aws = {
+#        source  = "hashicorp/aws"
+#        version = ">= 5.17.0"
+#      }
+#    }
+
+#    cloud {
+#    organization = "thevopz"
+
+#    workspaces {
+#      name = "root-tf"
+#    }
+#  }
 }
+
