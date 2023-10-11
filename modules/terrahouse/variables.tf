3edd58b867fc55d0544d7665c8484c9ecef3b035
sm1lexops <smiledevops@gmail.com>
2023-10-11 19:44:35 +0000

    fix bucket env var

--- modules/terrahouse/variables.tf
+++ modules/terrahouse/variables.tf
@@ -7,19 +7,19 @@ variable "create" {
   default       = true 
 }
 
-variable "bucket" {
-  description   = "The name of the s3 bucket"
-  type          = string
-  default       = ""
-
-  validation {
-    condition     = (
-      length(var.bucket) >= 10 && length(var.bucket) <= 60 && 
-      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket))
-    )
-    error_message = "The bucket name must be between 10 and 60 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
-  } 
-}
+#variable "bucket" {
+#  description   = "The name of the s3 bucket"
+#  type          = string
+#  default       = ""
+#
+#  validation {
+#    condition     = (
+#      length(var.bucket) >= 10 && length(var.bucket) <= 60 && 
+#      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket))
+#    )
+#    error_message = "The bucket name must be between 10 and 60 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+#  } 
+#}
 
 variable "tags" {
   description   = "A map of tags asign to the bucket"
